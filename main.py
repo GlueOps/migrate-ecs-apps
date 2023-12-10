44ce9c76ac999e91526f32004295d02c335e07c7
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-12-10 05:59:50 +0000

    feat: render templates with user prompt

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 
 from glueops.setup_logging import configure as go_configure_logging
 
-from src.load_vault import load_secrets_to_vault
+# from src.load_vault import load_secrets_to_vault
 from src.get_configs_from_ecs import get_configs_from_ecs
-# from src.create_app_configs.create_app_configs import render_app_configs
+from src.create_app_configs.create_app_configs import render_app_configs
+from src.stage_app_data import stage_app_data_from_csv
 
 # configure logger
 logger = go_configure_logging(
@@ -13,43 +14,51 @@ logger = go_configure_logging(
 )
 
 
-
 from pprint import pprint
 
-pprint(
-get_configs_from_ecs(
-        cluster_arn=os.environ['CLUSTER_ARN'],
-        service_arn=os.environ['SERVICE_ARN']
+csv_app_data = stage_app_data_from_csv('/app/inputs/glueops_wip.csv')
+
+for d in csv_app_data:
+    ecs_stage_conf = get_configs_from_ecs(
+        cluster_arn=os.environ['STAGE_CLUSTER_ARN'],
+        service_arn=d['stage_ecs_service']
     )
-)
+    ecs_prod_conf = get_configs_from_ecs(
+        cluster_arn=os.environ['PROD_CLUSTER_ARN'],
+        service_arn=d['prod_ecs_service']
+    )
+    app_config = {
+        "app_repo": d['app_repo'],
+        "ecr_repository": ecs_prod_conf['ecr_repository'],
+        "env_configs": [
+            {
+                "env": "stage",
+                "image_tag": ecs_stage_conf['image_tag'],
+                "hostnames": d['stage_hostnames'],
+                "volume_mount_claim_name": d['stage_volume_mount_claim_name'],
+                "volume_mount_path": ecs_stage_conf['volume_mount_path'],
+                "volume_mount_sub_path": ecs_stage_conf['volume_mount_sub_path'],
+                "web_acl_name": None,
+                "vault_secrets": ecs_stage_conf['vault_secrets']
+            },
+            {
+                "env": "prod",
+                "image_tag": ecs_prod_conf['image_tag'],
+                "hostnames": d['prod_hostnames'],
+                "volume_mount_claim_name": d['prod_volume_mount_claim_name'],
+                "volume_mount_path": ecs_prod_conf['volume_mount_path'],
+                "volume_mount_sub_path": ecs_prod_conf['volume_mount_sub_path'],
+                "web_acl_name": "primary",
+                "vault_secrets": ecs_prod_conf['vault_secrets']
+            }
+        ]
+    }
+    print(f'the following configurations are staged for {d["app_repo"]}')
+    pprint(app_config)
 
-test_app_config = {
-    "app_repo": "my-repository",
-    "ecr_repository": "my.ecr.repo",
-    "env_configs": [
-        {
-            "env": "stage",
-            "image_tag": "v0.1.0",
-            "hostnames": [
-                "stage.example.com",
-                "www.stage.example.com"
-            ],
-            # "volume_mount_claim_name": "my-claim",
-            # "volume_mount_sub_path": "my-sub-path"
-            # "web_acl_name": "my-web-acl"
-        },
-        {
-            "env": "prod",
-            "image_tag": "v0.1.0",
-            "hostnames": [
-                "example.com",
-                "www.example.com"
-            ],
-            "volume_mount_claim_name": "my-claim",
-            "volume_mount_sub_path": "my-sub-path",
-            "web_acl_name": "my-web-acl"
-        }
-    ]
-}
-
-# render_app_configs(test_app_config)
+    confirm = input('render templates and write secrets to vault (yolo/no): ').strip().lower()
+    if confirm == 'yolo':
+        print('rendering templates and writing secrets')
+        render_app_configs(app_config)
+    else:
+        print(f'skipping configuration of {d["app_repo"]}')
